8558eeaf22b73bc3b9a049acc16d3016d31def27
aristapanell-cell <aristapanell@gmail.com>
2026-07-02 00:09:54 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-apt update -y
-curl -o $PREFIX/bin/arista https://raw.githubusercontent.com/aristapanell-cell/AristaScanner/main/arista.sh
-chmod +x $PREFIX/bin/arista
+apt update -y 2>/dev/null
+curl -o $PREFIX/bin/arista https://raw.githubusercontent.com/aristapanell-cell/AristaScanner/main/arista.sh 2>/dev/null
+chmod +x $PREFIX/bin/arista 2>/dev/null
+echo -e "\033[1;32m✓ Arista Scanner installed successfully!\033[0m"
+echo -e "\033[1;36mRun 'arista' to start\033[0m"
